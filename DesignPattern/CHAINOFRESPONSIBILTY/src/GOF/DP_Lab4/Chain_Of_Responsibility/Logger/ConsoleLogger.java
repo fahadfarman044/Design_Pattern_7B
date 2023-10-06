241e83526cac595577f08b20909e529710e0330e
fahadfarman044 <fahadfarman4@gmail.com>
2023-10-06 09:13:38 +0500

    ok

---
+++ DesignPattern/CHAINOFRESPONSIBILTY/src/GOF/DP_Lab4/Chain_Of_Responsibility/Logger/ConsoleLogger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.DP_Lab4.Chain_Of_Responsibility.Logger;
+
+/**
+ *
+ * @author fa20-bse-044
+ */
+public class ConsoleLogger extends AbstractLogger {
+
+   public ConsoleLogger(int level){
+      this.level = level;
+   }
+
+   @Override
+   protected void write(String message) {		
+      System.out.println("Standard Console::Logger: " + message);
+   }
+}
---
+++ DesignPattern/CHAINOFRESPONSIBILTY/src/GOF/DP_Lab4/Chain_Of_Responsibility/Logger/FileLogger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.DP_Lab4.Chain_Of_Responsibility.Logger;
+
+/**
+ *
+ * @author fa20-bse-044
+ */
+public class FileLogger extends AbstractLogger {
+
+   public FileLogger(int level){
+      this.level = level;
+   }
+
+   @Override
+   protected void write(String message) {		
+      System.out.println("File::Logger: " + message);
+   }
+}
