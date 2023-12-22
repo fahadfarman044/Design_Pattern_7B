fb96ced68f9c7f153283ca82e804b8cf1a116adb
fahadfarman044 <fahadfarman4@gmail.com>
2023-12-22 09:59:27 +0500

    main

---
+++ DesignPattern/GOF Patterns/src/FactoryMethod/Refactoring_guru/Dialog.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryMethod.Refactoring_guru;
+
+/**
+ *
+ * @author fa20-bse-044
+ */
+public abstract class Dialog {
+
+    public void renderWindow() {
+        // ... other code ...
+
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    /**
+     * Subclasses will override this method in order to create specific button
+     * objects.
+     */
+    public abstract Button createButton();
+}
---
+++ DesignPattern/GOF Patterns/src/FactoryMethod/Refactoring_guru/HtmlButton.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryMethod.Refactoring_guru;
+
+/**
+ *
+ * @author fa20-bse-044
+ */
+public class HtmlButton implements Button {
+
+    public void render() {
+        System.out.println("<button>Test Button</button>");
+        onClick();
+    }
+
+    public void onClick() {
+        System.out.println("Click! Button says - 'Hello World!'");
+    }
+}
